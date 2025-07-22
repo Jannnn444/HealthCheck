55afbadf453fd814253554fc6112b8a07d93976b
Janus <Janus.Haung@hualiteq.com>
2025-07-22 13:56:12 +0800

    Initial Commit

---
+++ SendBloodPressure/SendBloodPressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendBloodPressureApp.swift
+//  SendBloodPressure
+//
+//  Created by Hualiteq International on 2025/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SendBloodPressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
